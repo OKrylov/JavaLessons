7de6838405e5787a7eceeed6990aca2a12f4b8f1
OKrylov <okrylov@axcient.com>
2021-12-03 02:09:39 +0300

    Added spring boot framework

---
+++ src/main/java/pro/skypro/course2/spring/SpringApp.java
@@ -0,0 +1,13 @@
+package pro.skypro.course2.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApp.class, args);
+    }
+
+}

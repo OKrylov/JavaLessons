35a6af6e1a651df1a946bd494b25118e47d5cdd6
OKrylov <okrylov@axcient.com>
2022-02-18 12:24:40 +0300

    test homework4

---
+++ src/main/java/pro/skypro/course2/testhomework/TestClass2.java
@@ -0,0 +1,9 @@
+package pro.skypro.course2.testhomework;
+
+public class TestClass2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println("Hello2!");
+    }
+}
